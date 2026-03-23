2b7d8b27c3efd8e237856f3922085d7db34a156b
allantristanbustamante <allantristanb@gmail.com>
2026-03-23 20:50:41 +0800

    Add files via upload

---
+++ VISUAL STUDIO_UNICONNECT_LOGIN PAGE/UNICONNECT1/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace UNICONNECT1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.pnlCard = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStudentId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.chkRemember = new System.Windows.Forms.CheckBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.footerLabel = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.infoDot = new System.Windows.Forms.Panel();
+            this.infoPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.pnlCard.SuspendLayout();
+            this.infoPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.pbLogo);
+            this.panel1.Controls.Add(this.pnlCard);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(896, 705);
+            this.panel1.TabIndex = 0;
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pbLogo.BackgroundImage = global::UNICONNECT1.Properties.Resources.LOGO_PIC;
+            this.pbLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pbLogo.Location = new System.Drawing.Point(0, 0);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(896, 81);
+            this.pbLogo.TabIndex = 1;
+            this.pbLogo.TabStop = false;
+            this.pbLogo.Click += new System.EventHandler(this.pbLogo_Click);
+            // 
+            // pnlCard
+            // 
+            this.pnlCard.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlCard.BackColor = System.Drawing.Color.White;
+            this.pnlCard.Controls.Add(this.label2);
+            this.pnlCard.Controls.Add(this.txtStudentId);
+            this.pnlCard.Controls.Add(this.label3);
+            this.pnlCard.Controls.Add(this.txtPassword);
+            this.pnlCard.Controls.Add(this.chkRemember);
+            this.pnlCard.Controls.Add(this.infoPanel);
+            this.pnlCard.Controls.Add(this.btnLogin);
+            this.pnlCard.Controls.Add(this.footerLabel);
+            this.pnlCard.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.pnlCard.Location = new System.Drawing.Point(237, 119);
+            this.pnlCard.Margin = new System.Windows.Forms.Padding(10);
+            this.pnlCard.Name = "pnlCard";
+            this.pnlCard.Padding = new System.Windows.Forms.Padding(30);
+            this.pnlCard.Size = new System.Drawing.Size(400, 500);
+            this.pnlCard.TabIndex = 0;
+            this.pnlCard.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(33, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "STUDENT ID NUMBER";
+            // 
+            // txtStudentId
+            // 
+            this.txtStudentId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtStudentId.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentId.Location = new System.Drawing.Point(33, 55);
+            this.txtStudentId.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.txtStudentId.Name = "txtStudentId";
+            this.txtStudentId.Size = new System.Drawing.Size(338, 29);
+            this.txtStudentId.TabIndex = 1;
+            this.txtStudentId.TextChanged += new System.EventHandler(this.txtStudentId_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(33, 107);
+            this.label3.Margin = new System.Windows.Forms.Padding(3, 20, 3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "PASSWORD";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(33, 132);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(338, 29);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // chkRemember
+            // 
+            this.chkRemember.AutoSize = true;
+            this.chkRemember.ForeColor = System.Drawing.Color.DimGray;
+            this.chkRemember.Location = new System.Drawing.Point(33, 179);
+            this.chkRemember.Margin = new System.Windows.Forms.Padding(3, 15, 3, 3);
+            this.chkRemember.Name = "chkRemember";
+            this.chkRemember.Size = new System.Drawing.Size(94, 17);
+            this.chkRemember.TabIndex = 4;
+            this.chkRemember.Text = "Remember me";
+            this.chkRemember.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.DarkRed;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(33, 267);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(338, 42);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "LOG IN";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // footerLabel
+            // 
+            this.footerLabel.ForeColor = System.Drawing.Color.DimGray;
+            this.footerLabel.Location = new System.Drawing.Point(33, 322);
+            this.footerLabel.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.footerLabel.Name = "footerLabel";
+            this.footerLabel.Size = new System.Drawing.Size(338, 30);
+            this.footerLabel.TabIndex = 6;
+            this.footerLabel.Text = "Forgot password? Contact the ICTD";
+            this.footerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.ForeColor = System.Drawing.Color.DimGray;
+            this.infoLabel.Location = new System.Drawing.Point(3, 33);
+            this.infoLabel.MaximumSize = new System.Drawing.Size(320, 0);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(320, 0);
+            this.infoLabel.TabIndex = 1;
+            this.infoLabel.Text = "Accessing this portal allows you to view your school\'s overall development for a " +
+    "specific semester.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Accessing this portal allows you to view your school\'s overall\r\ndevelopment for a" +
+    " specific semester.\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // infoDot
+            // 
+            this.infoDot.BackColor = System.Drawing.Color.DarkRed;
+            this.infoDot.Location = new System.Drawing.Point(3, 6);
+            this.infoDot.Margin = new System.Windows.Forms.Padding(3, 6, 6, 3);
+            this.infoDot.Name = "infoDot";
+            this.infoDot.Size = new System.Drawing.Size(10, 24);
+            this.infoDot.TabIndex = 0;
+            // 
+            // infoPanel
+            // 
+            this.infoPanel.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.infoPanel.AllowDrop = true;
+            this.infoPanel.Controls.Add(this.infoDot);
+            this.infoPanel.Controls.Add(this.label1);
+            this.infoPanel.Controls.Add(this.infoLabel);
+            this.infoPanel.Location = new System.Drawing.Point(33, 209);
+            this.infoPanel.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.infoPanel.Name = "infoPanel";
+            this.infoPanel.Size = new System.Drawing.Size(326, 35);
+            this.infoPanel.TabIndex = 5;
+            this.infoPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.infoPanel_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::UNICONNECT1.Properties.Resources.login_bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(896, 705);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.pnlCard.ResumeLayout(false);
+            this.pnlCard.PerformLayout();
+            this.infoPanel.ResumeLayout(false);
+            this.infoPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel pnlCard;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStudentId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.CheckBox chkRemember;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label footerLabel;
+        private System.Windows.Forms.FlowLayoutPanel infoPanel;
+        private System.Windows.Forms.Panel infoDot;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label infoLabel;
+    }
+}
+
