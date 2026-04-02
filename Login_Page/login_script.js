a31e67338620d51030cf578de93b1e0609d69b35
AdrianJamesTupas <adztaekwondo@gmail.com>
2026-04-02 20:20:55 +0800

    Delete Login_Page directory

--- Login_Page/login_script.js
+++
@@ -1,9 +0,0 @@
-const togglePassword = document.querySelector('#togglePassword');
-const password = document.querySelector('#password');
-
-togglePassword.addEventListener('click', function () {
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    
-    this.textContent = type === 'password' ? '⌣' : '👁';
-});
